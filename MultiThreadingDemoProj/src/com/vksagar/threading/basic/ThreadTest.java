7c91e506c4d0a6b919da5cc9631277d5e88284d4
Vikram Kshirsagar <kshirsagarvikram7@gmail.com>
2019-08-04 17:41:07 +0530

    Added join() method

--- MultiThreadingDemoProj/src/com/vksagar/threading/basic/ThreadTest.java
+++ MultiThreadingDemoProj/src/com/vksagar/threading/basic/ThreadTest.java
@@ -6,7 +6,7 @@ public class ThreadTest extends Thread {
 	public void run() {
 		for(int i=0; i<10; i++) {
 			try {
-				Thread.sleep(1000);
+				Thread.sleep(500);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -18,9 +18,24 @@ public class ThreadTest extends Thread {
 	public static void main(String[] args) {
 		ThreadTest t1 = new ThreadTest();
 		ThreadTest t2 = new ThreadTest();
+		ThreadTest t3 = new ThreadTest();
 		
 		t1.start();
+		
+		try {
+			t1.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		t2.start();
+		try {
+			t2.join(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		t3.start();
 	}
 
 }
